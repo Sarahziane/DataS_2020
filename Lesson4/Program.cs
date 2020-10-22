353e1724eb90a0695289c9cef358dc57448373c4
Sarah ZIANE <sarahziane5@gmail.com>
2020-10-22 12:58:30 +0300

    4

--- Lesson4/Program.cs
+++ Lesson4/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello 4!");
         }
     }
 }
